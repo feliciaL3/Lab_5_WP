6fcb32e6604b967034ca9af6668f1b1af7ce860d
Felicia <lupascu.felicia.2014@gmail.com>
2024-04-15 20:14:04 +0300

    go2web

--- go2web.py
+++ go2web.py
@@ -1,221 +1,245 @@
-import argparse
-import json
+import sys
 import socket
 import ssl
-from datetime import datetime, timedelta
-from urllib.parse import quote_plus
+import json
+import hashlib
+from urllib.parse import urlparse, quote
 from bs4 import BeautifulSoup
+from tinydb import TinyDB, Query
+
+cache_file = "cache.json"
+db = TinyDB(cache_file)
 
 
-# def extract_links_from_url(url, max_links=15):
-# try:
-# response = requests.get(url)
-#    response.raise_for_status()
-#    html_content = response.text
-#    soup = BeautifulSoup(html_content, 'html.parser')
-#    links = [link.get('href') for link in soup.find_all('a', href=True)[:max_links]]
-#    return links
-# except requests.RequestException as e:
-#    print("Error extracting links:", e)
-#    return None
+def hash_url(url):
+    return hashlib.md5(url.encode()).hexdigest()
 
 
-def make_https_request(url, max_redirects=5, accept="text/html"):
-    cached_data = get_cached_data(url)
-    if cached_data:
-        print("Using cached data for the url:", url)
-        print(cached_data)
-        return
-    #    links = extract_links_from_url(url)
-    # if links:
-    # print("Extracted links:")
-    # for link in links:
-    #    print(link)
-    # else:
-    #    print("Failed to extract links.")
-    #    return
+def cache_response(url, response):
+    if 'text/html' in response:
+        parsed_response = parse_html(response)
+    else:
+        parsed_response = response
 
-    url_parts = url.split('//')[-1].split('/')  # desparte URL-ul după primele două slash-uri // și ia partea de după ele
-    host = url_parts[0]  # host
-    path = '/' + '/'.join(url_parts[1:])  # pentru a forma calea completă a URL-ului
-    port = 443  # Default HTTPS port
+    db.insert({'url': hash_url(url), 'response': parsed_response})
 
-    # crearea socket
-    with socket.create_connection((host, port)) as client_socket:
-        with ssl.create_default_context().wrap_socket(client_socket, server_hostname=host) as secure_socket:
-            while max_redirects > 0:  # bucla, va itera până când (max_redirects) devine zero
-                # Construim cererea HTTP GET care urmează să fie trimisă la server.
-                request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nAccept: {accept}\r\nConnection: close\r\n\r\n"
-                secure_socket.sendall(request.encode())  # encode() folosit pentru transf șirul de caractere în octeți
 
-                response = b""  # ițializam var care va stoca răspunsul de la server
-                while True:  # Primirea datelor de la server în blocuri de 1024 de octeți
-                    part = secure_socket.recv(1024)
-                    if not part:  # verificam dacă ultimul bloc de date primit de la server e gol
-                        break
-                    response += part
+def is_cached(url):
+    return db.contains(Query().url == hash_url(url))
 
-                # Redirectionarea
-                print("**************************")
-                if response:  # divizam răspunsul în linii,apoi preia prima linie care conține status line
-                    status_line = response.split(b"\r\n", 1)[0].decode("utf-8")
-                    if status_line.startswith("HTTP/1.1"):  # răspuns HTTP valid
-                        # impărțim status line-ul în cuvinte (separând după spațiu) și preluăm al doilea cuvânt
-                        status_code = int(status_line.split()[1])
-                    else:
-                        print("Invalid HTTP response:", status_line)
-                        return
-                else:
-                    return
-                print("Status code:", status_code)
 
-                if 300 <= status_code < 400:
-                    max_redirects -= 1
-                    redirect_url = extract_redirect_url(response)
-                    if not redirect_url:
-                        print("error, no url found")
-                        break
-                    print("Redirecting to:", redirect_url)
-                    make_https_request(redirect_url)
-                else:
+def retrieve_cached_response(url):
+    result = db.get(Query().url == hash_url(url))
+    return result['response']
+
+
+def print_cached_response(response):
+    if isinstance(response, list):
+        for item in response:
+            print(item)
+    elif isinstance(response, str):
+        print("Modified JSON Response:")
+        try:
+            json_data = json.loads(response.split('\r\n\r\n', 1)[1])
+            print(json.dumps(json_data, indent=4))  # Print JSON data with indentation
+        except json.JSONDecodeError as e:
+            print("Error: Unable to parse JSON data:", e)
+    else:
+        print("Unknown response type")
+
+
+
+def extract_url_data(url):
+    parsed_url = urlparse(url)
+
+    port = None
+    if parsed_url.scheme == "https":
+        port = 443
+    elif parsed_url.scheme == "http":
+        port = 80
+
+    return parsed_url.netloc, port, parsed_url.path
+
+
+def make_http_request(url):
+    if is_cached(url):
+        print("Retrieving cached response:", url)
+        return retrieve_cached_response(url)
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    host, port, path = extract_url_data(url)
+    print("Establishing connection:", host, port, path)
+
+    if port == 443:
+        context = ssl.create_default_context()
+        client_socket = context.wrap_socket(client_socket, server_hostname=host)
+
+    try:
+        client_socket.settimeout(2)
+        client_socket.connect((host, port))
+
+        request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+        client_socket.send(request.encode())
+
+        response = b""
+        while True:
+            try:
+                data = client_socket.recv(1024)
+                if not data:
                     break
 
-    if 200 <= status_code < 300:
-        print("Success:")
+                response += data
+
+            except socket.timeout:
+                break
+
+        resp_data = response.decode('utf-8', errors='ignore')
+        cache_response(url, resp_data)
+
+        return resp_data
+
+    finally:
+        client_socket.close()
+
+
+def handle_html_or_json(url):
+    if is_cached(url):
+        print("Retrieving cached response for:", url)
+        response = retrieve_cached_response(url)
+        print_cached_response(response)
+        return response
     else:
-        print("Failure:")
-
-    # content type from header
-    headers = response.split(b"\r\n\r\n", 1)[0].decode("utf-8")
-    # print("Response headers:", headers)
-    content_type = None
-    for header in headers.split("\r\n"):
-        if header.startswith("Content-Type:"):
-            content_type = header.split(":")[1].strip()
-            break
-
-    # process the response based on content type html or json
-    if content_type == "application/json":
-        json_content = json.loads(response.split(b"\r\n\r\n", 1)[1].decode("utf-8"))
-        print(json_content)
-        relevant_content = json_content
-    else:
-        try:
-            html_content = response.decode('utf-8')
-        except UnicodeDecodeError:
-            html_content = response.decode('ISO-8859-1')
-
-        # extract the content
-        soup = BeautifulSoup(html_content, 'html.parser')
-        relevant_content = soup.get_text()
-
-        print(relevant_content)
-
-    # cache the fetched data
-    cache_data(url, relevant_content)
-
-
-# load cached data from a JSON file
-def load_cache():
-    try:
-        with open("cache.json", "r") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
-
-
-# save cached data to file
-def save_cache(cache):
-    with open("cache.json", "w") as f:
-        json.dump(cache, f, indent=4)
-
-
-# retrieve data from file
-def get_cached_data(url):
-    cache = load_cache()
-    cached_data = cache.get(url)
-    if cached_data:
-        cached_time = datetime.fromisoformat(cached_data["timestamp"])
-        if datetime.now() - cached_time < timedelta(hours=1):
-            return cached_data["content"]
+        response = make_http_request(url)
+        if 'text/html' in response:
+            try:
+                parsed_response = parse_html(response)
+                cache_response(url, parsed_response)
+                return parsed_response
+            except Exception as e:
+                print("Error: Unable to parse HTML data:", e)
+        elif 'application/json' in response:
+            try:
+                json_data = json.loads(response.split('\r\n\r\n', 1)[1])
+                print("Modified JSON Response:")
+                print(json.dumps(json_data, indent=4))  # Modified JSON output
+                return json_data
+            except json.JSONDecodeError as e:
+                print("Error: Unable to parse JSON data:", e)
         else:
-            # delete expired content from file
-            del cache[url]
-            save_cache(cache)
-    return None
+            print(response)
 
 
-# cache fetched data
-def cache_data(url, content):
-    cache = load_cache()
-    cache[url] = {"timestamp": datetime.now().isoformat(), "content": content}
-    save_cache(cache)
+def parse_html(response):
+    soup = BeautifulSoup(response, 'html.parser')
+    all_elements = soup.find_all(['h1', 'h2', 'h3', 'p'])
+    all_info = []
+    for element in all_elements:
+        if element.name.startswith('h'):
+            depth = len(element.name) - 1  # Depth based on heading level
+            tag = f"[{element.name}]"
+            stars = '*' * (4 - depth)  # Adjusting the number of asterisks
+            all_info.append(f"{stars} {tag} {element.get_text()}")  # Customized representation
+        elif element.name == 'p':
+            tag = f"[{element.name}]"
+            all_info.append(f"* {tag} {element.get_text()}")
+
+    links = soup.find_all('a', href=True)
+    links_href = [link['href'] for link in links if link['href'].startswith('http')]
+    all_info.append("-- Links --")
+    all_info += links_href
+
+    return all_info
 
 
-def extract_redirect_url(response):
-    headers, _, body = response.partition(b'\r\n\r\n')
-    for line in headers.split(b'\r\n'):
-        if line.startswith(b'Location:'):
-            link = line.split(b':', 1)[1].strip().decode()
-            return link
-    return None
-
-
-def search_google(search_term):
+def make_google_search(search_term):
+    host = "www.google.com"
+    search_query = quote(search_term)
+    path = f"/search?q={search_query}"
     try:
-        encoded_search = quote_plus(search_term)
-        request_url = f"/search?q={encoded_search}"
-
-        with socket.create_connection(("www.google.com", 80)) as client_socket:
-            request = f"GET {request_url} HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n"
-            client_socket.sendall(request.encode())
-
-            response = b""
+        context = ssl.create_default_context()
+        with context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=host) as s:
+            s.connect((host, 443))
+            request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
+            s.sendall(request.encode())
+            response = b''
             while True:
-                part = client_socket.recv(1024)
-                if not part:
+                data = s.recv(1024)
+                if not data:
                     break
-                response += part
-
-        try:
-            html_content = response.decode('utf-8')
-        except UnicodeDecodeError:
-            html_content = response.decode('ISO-8859-1')
-
-        soup = BeautifulSoup(html_content, 'html.parser')
-        count = 0
-        for index, div in enumerate(soup.find_all('div', class_='egMi0 kCrYT'), 1):
-            link_element = div.find('a', href=True)
-            title_element = div.find('h3', class_='zBAuLc l97dzf')
-            if link_element and title_element:
-                title = title_element.get_text(strip=True)
-                link = link_element['href']
-
-                # extract only link from "http" until "/&"
-                start_index = link.find("http")
-                end_index = link.find("&")
-                if start_index != -1 and end_index != -1:
-                    link = link[start_index:end_index]
-
-                print(" * \033[92m", title, "\033[0m",  " - ", link)
-
-                count += 1
-                if count >= 10:
+                response += data
+        response_str = response.decode("utf-8", errors="ignore")
+        soup = BeautifulSoup(response_str, 'html.parser')
+        links = soup.find_all('a')
+        search_results = []
+        for link in links:
+            href = link.get('href')
+            if href.startswith('/url?q='):
+                url = href.split('/url?q=')[1].split('&')[0]
+                title = link.get_text()  # Extrage titlul linkului
+                search_results.append((title, url))
+                if len(search_results) >= 10:
                     break
-
+        return search_results
     except Exception as e:
-        print("Error:", e)
+        return f"Error: {str(e)}"
+
+
+def search(term):
+    search_results = make_google_search(term)
+    if isinstance(search_results, list):
+        print("Search results for", term, ":")
+        for i, (title, url) in enumerate(search_results, start=1):
+            print(f"{i}. \033[92m{title}\033[0m: {url}")
+    else:
+        print("Error:", search_results)
+
+
+def print_error():
+    print("No option provided.")
+    print("Usage: ")
+    print("python go2web.py -u URL")
+    print("python go2web.py -s SEARCH_TERM")
+    print("python go2web.py -h")
+    sys.exit()
+
+
+def main():
+    args = sys.argv[1:]
+
+    if not args:
+        print_error()
+
+    if '-u' in args:
+        url_index = args.index('-u') + 1
+        if url_index < len(args):
+            url = args[url_index]
+            response = handle_html_or_json(url)
+            print("Information extracted from", url, ".")
+            if isinstance(response, list):
+                for info in response:
+                    print(info)
+            elif isinstance(response, dict):
+                pass
+        else:
+            print("Error: No URL provided after -u")
+            sys.exit()
+
+    elif '-s' in args:
+        search_index = args.index('-s') + 1
+        if search_index < len(args):
+            term = ' '.join(args[args.index('-s') + 1:]) if '-s' in args else None
+            if term:
+                search(term)
+            else:
+                print("Error: No search term provided after -s")
+                sys.exit()
+
+    elif '-h' in args:
+        print("go2web -u <URL>         # Make an HTTP request to the specified URL and print the response")
+        print("go2web -s <search-term> # Make an HTTP request to search the term  and print top 10 results")
+        print("go2web -h               # Show this help guide ")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Creating HTTP requests and retrieving content from websites")
-    parser.add_argument("-u", "--url", help="Make an HTTP request to the specified URL and print the response")
-    parser.add_argument("-s", "--search", help="Make an HTTP request to search the term  and print top 10 results")
-    args = parser.parse_args()
-
-    if args.url:
-        make_https_request(args.url)
-    elif args.search:
-        search_google(args.search)
-    else:
-        print("Input parameter")
+    main()
